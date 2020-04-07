b04d1d508a7d9330fab81514bcec19ca471d16d9
harvinder <harvinder.singh@resmed.com.au>
2020-04-07 11:46:16 +1000

    Initial commit

---
+++ App6/Views/BlankPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using App6.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace App6.Views
+{
+    public sealed partial class BlankPage : Page
+    {
+        public BlankViewModel ViewModel { get; } = new BlankViewModel();
+
+        public BlankPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
